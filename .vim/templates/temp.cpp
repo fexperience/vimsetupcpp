74b412abae4f9032f1cc2a70ec002df0108d2427
fexperience <152182435+fexperience@users.noreply.github.com>
2025-05-12 20:18:20 +0700

    Create temp.cpp

---
+++ .vim/templates/temp.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define MAXN 1000006
+#define maxn 100005
+#define f0(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fopenn(file) if(fopen(file".inp", "r")){freopen(file".inp","r",stdin);freopen(file".out","w",stdout);}
+#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int main(){
+    io;
+    //fopenn("namefile");
+    
+    
+    
+    cerr << '\n';
+    return 0;
+}
